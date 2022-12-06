d28765e6ebfd5b2bb4a1b26d5dac8d42d752f04e
Richard Batyrov <batyrov@iteco-inno.ru>
2022-12-06 09:44:43 +0300

    blank message

---
+++ untitled/src/main/java/com/richardbatyrov/jenkinsDockerSample/App.java
@@ -0,0 +1,8 @@
+package com.richardbatyrov.jenkinsDockerSample;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hi! This is sample docker app for jenkins");
+    }
+}
